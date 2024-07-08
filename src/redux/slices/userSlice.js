119b319df3b7f53da552df7e43db1259754c8888
Shivam Sharma <shivsharma7821@gmail.com>
2024-07-08 21:32:24 +0530

    Created Folder structure

---
+++ src/redux/slices/userSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const userSlice = createSlice({
+    userData: {},
+    initialState: null,
+    reducers: {
+    },
+});
+
+export const {  } = userSlice.actions;
+export default userSlice.reducer;
